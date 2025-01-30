2b3f29e780e535e292b5f2f8e0c501bf23761f8e
Marcio Ferreira <marcio.ferreira@itarget.com.br>
2025-01-30 16:36:06 -0300

    Ajustes

--- frontend/src/pages/Professores/index.tsx
+++ frontend/src/pages/Professores/index.tsx
@@ -4,14 +4,20 @@ import vivi from '../../assets/professores/vivi_santos.jpg'
 import bruno from '../../assets/professores/bruno_jj.jpg'
 import matheus from '../../assets/professores/matheus_black_JIU JITSU.jpg'
 import paulo from '../../assets/professores/paulo_anderson.jpg'
+import yuri from '../../assets/professores/yuri.jpg'
+import renam from '../../assets/professores/renam.jpg'
+import leandro from '../../assets/professores/leandro.jpg'
 
 const professores = [
     { nome: 'Danilson - LPO/Musculação', imagem: danilson },
     { nome: 'Karyna - Coach Cross', imagem: karyna },
     { nome: 'Vivi Santos - Coach Cross', imagem: vivi },
+    { nome: 'Yuri - Coach Cross', imagem: yuri },
+    { nome: 'Leandro - Musculação|karatê', imagem: leandro },
+    { nome: 'Renan - Musculação', imagem: renam },
     { nome: 'Bruno - Jiu Jitsu (Kids)', imagem: bruno },
     { nome: 'Matheus Black - Jiu Jitsu', imagem: matheus },
-    { nome: 'Paulo - Muay Thai', imagem: paulo },
+    { nome: 'Paulo - Muay Thai', imagem: paulo }
 ]
 
 export default function Professores() {
